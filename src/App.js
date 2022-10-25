026185d05792fb1f9ab575fd45e433f9452bfa68
RohanKumar01 <rohankumar25601@gmail.com>
2022-10-26 01:08:57 +0530

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About  from './components/About';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
-  );
+  return ( 
+    <>
+    {/* <Navbar></Navbar>
+    <h1>thid is inotebook</h1> */}
+    <Router>
+      <Navbar /> 
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About/>
+        </Route> 
+      </Routes>
+    </Router>
+  </>
+);
 }
 
 export default App;
