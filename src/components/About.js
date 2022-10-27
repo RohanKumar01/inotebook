7db22489c8cac2a6e8e6f66f41f57790fa72c69e
RohanKumar01 <rohankumar25601@gmail.com>
2022-10-27 17:07:16 +0530

    Added option to read Notes

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,11 @@ import React from 'react'
 import { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 
-
  const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
-  
+
   return (
     <div>
-      This is about {a.state.name} ans he's in {a.state.class}
+      This is about
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, {useEffect} from 'react'
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+
+  let location = useLocation();
+  useEffect(() => {
+    console.log(location.pathname);
+    }, [location]);
+
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
               iNotebook
@@ -25,14 +31,10 @@ const Navbar = () => {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">
-                    Home
-                  </Link>
+                <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/about">
-                    About
-                  </Link>
+                <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} to="/about">About</Link>
                 </li>
                 {/* <li className="nav-item dropdown"> */}
                    {/* <Link
